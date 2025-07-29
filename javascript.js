3a1bcafc5ccf103a9e7b4ec348fd966363b3ca53
abubakerasifdar <abubakerasifdar100@gmail.com>
2025-07-29 15:44:43 +0500

    Functionality Feature is added successfully.

--- javascript.js
+++ javascript.js
@@ -1,20 +1,27 @@
 
-        document.addEventListener("keydown", (e) => {
-            if (e.key == "Enter") {
-                myFunction();
+document.addEventListener("keydown", (e) => {
+    if (e.key == "Enter") {
+        myFunction();
 
-            }
-        })
-        function myFunction() {
-            let input = document.getElementById('input').value;
-            let result = "";
-            let count=0;
-            for (let i = 0; i<input; i++){
-                count = count + i + '<br>' ;
-              
-                console.log(count);
-            }
-           result = count;
-            document.getElementById('output').style.display = "flex";
-            document.getElementById('output').innerHTML = result;
+    }
+})
+function myFunction() {
+    let input = Number(document.getElementById('input').value);
+    let result = "";
+    let count = 0;
+    let oddProduct = 1;
+    var oddNumber = '';
+    for (let i = 0; i <= input; i++) {
+        count = `${count + i},`;
+    }
+    let countarray = count.split(',');
+    let len = countarray.length-1
+    for (let i = 0; i < len; i++) {
+         if (countarray[i] != 0 && countarray[i] % 2 != 0) { oddNumber = oddNumber + Number(countarray[i]) + ",";
+            oddProduct = oddProduct * countarray[i];
         }
+    }
+    result = `You Entered ${input} and in this odd numbers: ${oddNumber} & Their Product is ${oddProduct}`;
+    document.getElementById('output').style.display = "flex";
+    document.getElementById('output').innerHTML = result;
+}
